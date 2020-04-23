014353a5a89f52998fd47829faf3bc394db7e250
Stefan <bruthaler@protonmail.com>
2020-04-23 11:05:14 +0200

    Adds docstring coverage

--- docstr_badge.sh
+++ docstr_badge.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 COV=$(docstr-coverage tinydb_encrypted_jsonstorage/ | grep "Total docstring coverage:" | cut -d":" -f 2 | cut -d";" -f1 | cut -d" " -f 2)
-python -m pybadges --left-text=docstr-coverage --right-text=$COV --right-color='#4c1' > ./badges/doc_coverage.svg
+CMP=$(echo $COV | cut -d"." -f 1)
+if [ $CMP -lt 50 ] 
+then
+	COLOR=#EF5350
+elif [ $COV -lt 90 ]
+then
+	COLOR=#FFCA28
+else 
+	COLOR=#66BB6A	
+fi
+
+
+python -m pybadges --left-text=docstr-coverage --right-text=$COV --right-color=$COLOR > ./badges/doc_coverage.svg
